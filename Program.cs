f6b44f6ffc4335b87ea33ed7a9484ed9fae18d6a
ravikandel1 <ravkdl@hotmail.com>
2017-10-06 22:47:18 +0545

    final

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            long f1 = 1, f2 = 2, f3 = 0,total=0;
+            do
+            {
+                f3 = f1 + f2;
+                if (f3 % 2 == 0)
+                {
+                    total += f3;
+                }
+                f1 = f2;
+                f2 = f3;
+            } while (f3 <= 4000000);
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
